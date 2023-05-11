6e2a1b4f63bd5d237a8046fae20f05b2d6390631
pedro kuchpil <pedrokuchpil@gmail.com>
2023-05-11 18:58:09 -0300

    Add SendGrid email via API

--- CallAPI.cs
+++ CallAPI.cs
@@ -1,47 +1,23 @@
 using Newtonsoft.Json.Linq;
+using SendGrid;
+using SendGrid.Helpers.Mail;
 
 namespace Broker
 {
     public class CallAPI
     {
-        public async Task checkValue(Asset asset)
+        public async Task<Double> checkValue(Asset asset)
         {
-            var email = new Email();
             HttpClient client = new HttpClient { BaseAddress = new Uri("https://brapi.dev/api/quote/") };
 
-            while(true)
-            {
-                var result = await client.GetAsync(asset.name);
-                
-                var json = await result.Content.ReadAsStringAsync();
-                var root = JObject.Parse(json);
-                
-                double value = root["results"][0]["regularMarketPrice"].ToObject<double>();
+            var result = await client.GetAsync(asset.name);
+            
+            var json = await result.Content.ReadAsStringAsync();
+            var root = JObject.Parse(json);
+            
+            double value = root["results"][0]["regularMarketPrice"].ToObject<double>();
 
-                if (value >= asset.sell_value && email.status != "SENT_SELL")
-                {
-                    Console.WriteLine($"Vender {asset.name}");
-                    email.status = "SENT_SELL";
-                }
-                else if (value <= asset.buy_value && email.status != "SENT_BUY")
-                {
-                    Console.WriteLine($"Comprar {asset.name}");
-                    email.status = "SENT_BUY";
-                }
-                else if (value < asset.sell_value && value > asset.buy_value)
-                {   
-                    if (email.status != "NONSENT")
-                    {
-                        Console.WriteLine($"O valor da ação {asset.name} não corresponde mais ao desejado.");
-                        email.status = "NONSENT";
-                    }
-                    else
-                    {
-                        Console.WriteLine($"Não fazer nada com {asset.name}");
-                    }
-                }
-                Thread.Sleep(10000);
-            }
+            return value;
             
         }
     }
--- Email.cs
+++ Email.cs
@@ -1,7 +1,25 @@
+using System.Net;
+using SendGrid;
+using SendGrid.Helpers.Mail;
+
 namespace Broker
 {
     public class Email
     {
-         public string status { get; set; } = "NONSENT";
+        public string status { get; set; } = "NONSENT";
+        public string apiKey { get; set; }
+        public SendGridClient client { get; set; }
+        public Email (string apiKey)
+        {
+            this.apiKey = apiKey;
+            this.client = new SendGridClient(apiKey);
+        }
+        public async Task sendEmail(string receiverEmail, string emailSubject, string emailContent, string htmlContent)
+        {
+            var senderEmail = new EmailAddress("pedrokuchpil@gmail.com", "Broker Pedro");
+            var receiver = new EmailAddress(receiverEmail);
+            var msg = MailHelper.CreateSingleEmail(senderEmail, receiver, emailSubject, emailContent, htmlContent);
+            var resp = await client.SendEmailAsync(msg).ConfigureAwait(false);
+        }
     }
 }    
